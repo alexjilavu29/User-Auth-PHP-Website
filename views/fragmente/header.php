a7b6a166a564b4190a998c5b625ca704365c442f
alexjilavu29 <119758927+alexjilavu29@users.noreply.github.com>
2023-01-21 12:54:17 +0200

    Add files via upload

---
+++ views/fragmente/header.php
@@ -0,0 +1,40 @@
+<header>
+            <a href="index.html">
+            <img src="./resurse/imagini/image.png" class="header-img">
+        </a>
+            <h1 class="header-titlu"> ASTRO PRODUCTIONS</h1>
+        </header>
+        <nav>
+            <label id="hamburger" for="ch-menu">
+                <img src="resurse/imagini/Hamburger_icon_alb.png" alt="menu">
+            </label>
+            <input id="ch-menu" type="checkbox">
+            <ul class="meniu">
+                <li>
+                    <div>Acasa</div>
+                    <ul>
+                        <li><a href="#welcome">Bine ati venit</a></li>
+                    </ul>
+
+
+                </li>
+                <li><a href="#">Inregistrare</a></li>
+                <li>
+                    <div>Despre</div>
+                    <ul>
+                        <li>Prezentare</li>
+                        <li>Istoric</li>
+                        <li>Motivatie</li>
+                    </ul>
+                </li>
+                <li>
+                    <div>Produse</div>
+                    <ul>
+                        <li>Toate</li>
+                    </ul>
+
+                </li>
+                <li>Contact</li>
+            </ul>
+            <img src="resurse/imagini/login.png" class="login-img">
+        </nav>
