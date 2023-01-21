98c6b91c627b2ad9411ae1306eaae6844d37fc08
alexjilavu29 <119758927+alexjilavu29@users.noreply.github.com>
2023-01-22 01:03:26 +0200

    Add files via upload

--- views/fragmente/head.php
+++ views/fragmente/head.php
@@ -1,14 +1,15 @@
 <meta charset="UTF-8">
         <meta name="description" content="Astro Productions - Cunoaste artisti noi de pretutindeni">
-        <link rel="shortcut icon" href="./resurse/imagini/favicon.ico" type="image/x-icon"/>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-        <link rel="manifest" href="/site.webmanifest">
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+        <link rel="shortcut icon" href="../../../../../resurse/imagini/favicon.ico" type="image/x-icon"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="../../../../../resurse/imagini/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="../../../../../resurse/imagini/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="../../../../../resurse/imagini/favicon-16x16.png">
+        <link rel="manifest" href="../../../../../resurse/imagini/site.webmanifest">
+        <link rel="mask-icon" href="../../../../../resurse/imagini/safari-pinned-tab.svg" color="#5bbad5">
         <meta name="msapplication-TileColor" content="#da532c">
         <meta name="theme-color" content="#ffffff">
 
-        <link rel="stylesheet" href="./resurse/css/general.css" type="text/css"/>
-        <link rel="stylesheet" href="./resurse/css/hamburger.css" type="text/css"/>
-        <link rel="stylesheet" href="./resurse/css/header.css" type="text/css"/>
+        <link rel="stylesheet" href="../../../../../resurse/css/general.css" type="text/css"/>
+        <link rel="stylesheet" href="../../../../../resurse/css/hamburger.css" type="text/css"/>
+        <link rel="stylesheet" href="../../../../../resurse/css/header.css" type="text/css"/>
+        <link rel="stylesheet" href="../../../../../resurse/css/inregistrare.css" type="text/css"/>
--- views/fragmente/header.php
+++ views/fragmente/header.php
@@ -1,40 +1,33 @@
-<header>
-            <a href="index.html">
-            <img src="./resurse/imagini/image.png" class="header-img">
+
+        <header>
+            <a href="../../../../../index.php">
+            <img src="../../../../../resurse/imagini/image.png" class="header-img">
         </a>
             <h1 class="header-titlu"> ASTRO PRODUCTIONS</h1>
         </header>
-        <nav>
-            <label id="hamburger" for="ch-menu">
-                <img src="resurse/imagini/Hamburger_icon_alb.png" alt="menu">
+        <nav >
+            <label id="hamburger" for="ch-menu" >
+                <img src="../../../../resurse/imagini/Hamburger_icon_alb.png" alt="menu">
             </label>
             <input id="ch-menu" type="checkbox">
             <ul class="meniu">
+                <li><a href="../../../../../index.php">
+                    Acasa
+                </a></li>
+                <li><a href="../../../../../views/pagini/inregistrare.php">Inregistrare</a></li>
                 <li>
-                    <div>Acasa</div>
-                    <ul>
-                        <li><a href="#welcome">Bine ati venit</a></li>
-                    </ul>
-
-
-                </li>
-                <li><a href="#">Inregistrare</a></li>
-                <li>
-                    <div>Despre</div>
+                    <div><a href="../../../../../views/pagini/despre.php">Despre</a></div>
                     <ul>
                         <li>Prezentare</li>
                         <li>Istoric</li>
                         <li>Motivatie</li>
                     </ul>
                 </li>
-                <li>
-                    <div>Produse</div>
-                    <ul>
-                        <li>Toate</li>
-                    </ul>
-
+                <li><a href="../../../../../views/pagini/artisti.php">
+                    Artiștii noștri</a>
                 </li>
-                <li>Contact</li>
+                <li>Abonează-te la Newsletter!</li>
             </ul>
-            <img src="resurse/imagini/login.png" class="login-img">
+            <a href="../../../../../views/pagini/inregistrare.php">
+                <img src="../../../../../resurse/imagini/login2.png" class="login-img"></a>
         </nav>
