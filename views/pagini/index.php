92c5d6e5418681c3ad9e07b0bcf1ee765c3fdee3
alexjilavu29 <119758927+alexjilavu29@users.noreply.github.com>
2023-01-21 12:53:59 +0200

    Add files via upload

---
+++ views/pagini/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="ro">
+    <head>
+        <title>Astro Productions</title>    
+        <?php include './views/fragmente/head.php';?>
+
+    </head>
+    <body>
+        <?php include './views/fragmente/header.php';?>
+        <main>
+            <section id="despre">
+              <h2>Despre ASTRO Productions</h2>
+              <p>Suntem o companie de producție muzicală care se specializează în crearea de muzică de înaltă calitate pentru o varietate de medii. Indiferent dacă căutați un coloana sonoră personalizată pentru filmul sau jocul video dvs., sau un sunet unic și profesionist pentru podcast-ul sau canalul YouTube, vă acoperim.</p>
+            </section>
+            <section id="servicii">
+              <h2>Serviciile noastre</h2>
+              <ul>
+                <li>Crearea coloanei sonore personalizate</li>
+                <li>Producție muzicală</li>
+                <li>Design sonor</li>
+                <li>Mixaj și mastering</li>
+              </ul>
+            </section>
+            <section id="portofoliu">
+              <h2>Portofoliul nostru</h2>
+              <ul>
+                <li>Partitură pentru filmul "Marea aventură"</li>
+                <li>Coloană sonoră pentru jocul video "Escape the City"</li>
+                <li>Muzică de intro pentru podcast-ul "The Daily Brief"</li>
+                <li>Tema canalului YouTube pentru "Cooking with Karen"</li>
+              </ul>
+            </section>
+            <section id="contact">
+                <h2>Contactați-ne</h2>
+                <p>Email: contact@astroproductions.com</p>
+                <p>Telefon: 555-555-5555</p>
+                <p>Adresa: Str. Principala 123, Etaj 456, Orasul X 0000</p>
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+              </section>
+          </main>
+          <?php include './views/fragmente/footer.php';?>
+</html>
