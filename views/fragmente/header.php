1f1520bb53420a443f0b17aabd0b7b0ae1cd8936
alexjilavu29 <119758927+alexjilavu29@users.noreply.github.com>
2023-01-22 23:36:20 +0200

    Add files via upload

--- views/fragmente/header.php
+++ views/fragmente/header.php
@@ -1,40 +1,45 @@
 <header>
-            <a href="index.html">
-            <img src="./resurse/imagini/image.png" class="header-img">
+    <?php session_start(); ?>
+            <a href="../../../../../index.php">
+            <img src="../../../../../resurse/imagini/image.png" class="header-img">
         </a>
             <h1 class="header-titlu"> ASTRO PRODUCTIONS</h1>
         </header>
-        <nav>
-            <label id="hamburger" for="ch-menu">
-                <img src="resurse/imagini/Hamburger_icon_alb.png" alt="menu">
+        <nav >
+            <label id="hamburger" for="ch-menu" >
+                <img src="../../../../resurse/imagini/Hamburger_icon_alb.png" alt="menu">
             </label>
             <input id="ch-menu" type="checkbox">
             <ul class="meniu">
-                <li>
-                    <div>Acasa</div>
-                    <ul>
-                        <li><a href="#welcome">Bine ati venit</a></li>
-                    </ul>
+                <li><a href="../../../../../index.php">
+                    Acasa
+                </a></li>
 
 
-                </li>
-                <li><a href="#">Inregistrare</a></li>
+
+                
+              <?php if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == 1): ?>
+                <li><a href="../../../../../views/pagini/profil.php">Profil</a></li>
+              <?php else: ?>
+                <li><a href="../../../../../views/pagini/inregistrare.php">Inregistrare</a></li>
+              <?php endif; ?>
+            
+                
+
+
                 <li>
-                    <div>Despre</div>
+                    <div><a href="../../../../../views/pagini/despre.php">Despre</a></div>
                     <ul>
                         <li>Prezentare</li>
                         <li>Istoric</li>
                         <li>Motivatie</li>
                     </ul>
                 </li>
-                <li>
-                    <div>Produse</div>
-                    <ul>
-                        <li>Toate</li>
-                    </ul>
-
+                <li><a href="../../../../../views/pagini/artisti.php">
+                    Artiștii noștri</a>
                 </li>
-                <li>Contact</li>
+                <li><a href="../../../../../views/pagini/newsletter.php">Abonează-te la Newsletter!</a></li>
             </ul>
-            <img src="resurse/imagini/login.png" class="login-img">
+            <a href="../../../../../views/pagini/inregistrare.php">
+                <img src="../../../../../resurse/imagini/login2.png" class="login-img"></a>
         </nav>
