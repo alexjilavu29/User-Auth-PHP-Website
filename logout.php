9a3d51bd1f8d131c57d1169cd698fb3987382ea6
alexjilavu29 <119758927+alexjilavu29@users.noreply.github.com>
2023-02-05 00:19:28 +0200

    Add files via upload

--- logout.php
+++ logout.php
@@ -1,5 +1,12 @@
 <?php
 session_start();
+$con = mysqli_connect("localhost", "astro_user", "VuEpEARll3ReG", "astro_proiectdaw", "3306");
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        exit();
+    }
+$insert_login = "UPDATE users SET logged_in = 0 WHERE email = '" . $_SESSION['email'] . "'";
+            mysqli_query($con, $insert_login);
 session_destroy();
 header("Location: index.php");
 exit;
